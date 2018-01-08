edd221c23852006d35d75f872680349113e71890
Park <parktesla@gmail.com>
2018-01-08 12:59:06 +0700

    แก้ icon shopping-basket

--- views/Themes/mobile/pages/orders/lists/json.php
+++ views/Themes/mobile/pages/orders/lists/json.php
@@ -17,7 +17,7 @@ foreach ($this->results['lists'] as $key => $value) {
 				'<div class="spacer"></div>'.
 					'<div class="massages">'.
 					'<div class="ui-score"></div>'.
-					'<div class="title fwb"><i class="icon-shopping-basket"></i> '.$value['code'].'</div>'.
+					'<div class="title fwb"><i class="icon-shopping-cart"></i> '.$value['code'].'</div>'.
 					'<div class="fwn"><i class="icon-clock-o"></i> '.date("d/m/Y", strtotime($value['date'])).'</div>'.
 					'<div class="fwb" style="color:red;"><i class="icon-money"></i> '.number_format($value['net_price'], 2).' ฿ </div>'.
 				'</div>'.
